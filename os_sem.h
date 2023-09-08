15f1ab429124dc161df8d7d6e46961a096cb8bf0
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-08 13:11:46 -0700

    semaphroe try prototype

--- os_sem.h
+++ os_sem.h
@@ -4,13 +4,20 @@
 #include "global_includes.h"
 /**
  * @brief Initializes a semaphore module
- * @param os_sem_t
+ * @param os_sem_t semaphore module
  */
 int os_sem_init(os_sem_t *sem, int count);
 
 /**
+ * @brief Weak attempt at trying to grab a semaphore
+ * @param os_sem_t
+ */
+int os_sem_try_entry(os_sem_t *sem);
+
+/**
  * @brief Allows us to acquuire our semaphore
  * @param os_sem_t
+ * @param uint32_t timeout for waiting
  */
 int os_sem_entry(os_sem_t *sem, uint32_t timeout_ms);
 
